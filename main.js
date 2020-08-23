42eb31668d8e8e28e26f9256b796b5ca7d190799
Claudia Melo <claudiamelocg@gmail.com>
2020-08-23 07:52:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const funcoes = require('./util/funcoes');
+const colors = require('colors'); //dependencia color
+
+
+console.log('Hello World' + funcoes.max(2,3));
+console.log('hello'.green);
+console.log('hello'.underline.red);
+console.log('hello'.inverse);
+console.log('hello'.rainbow);
